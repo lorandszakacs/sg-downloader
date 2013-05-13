2f4e20f33e66e0ccc531edb41e122cf3a3863b93
Lorand Szakacs <lorand.szakacs@gmail.com>
2013-05-13 14:17:33 -0500

    implemented a smarter argument parser

--- src/main/scala/home/sg/app/Main.scala
+++ src/main/scala/home/sg/app/Main.scala
@@ -12,32 +12,48 @@ import home.sg.client.HttpClientException
 import home.sg.client.UnknownSGException
 import home.sg.client.SGClient
 
+private class Args(val update: Boolean, val user: String, val pwd: String, val downloadPath: String, val sgs: List[String]) {
+}
+
+private object Args {
+  private val updateFlag = "--u"
+  private val passwordFlag = "--p"
+
+  def parseArgs(args: Array[String]): Args = {
+    val update = args.contains(updateFlag)
+    val conf = ConfigFactory.load()
+    val user = conf.getString(PropertyKeys.user)
+
+    val pwd = args.filter(a => a.contains(passwordFlag))
+    assume(pwd.length == 1, "You specified too many, or no password flags")
+    val password = pwd.head.takeRight(pwd.head.length - passwordFlag.length)
+
+    val downloadPath = if (update) conf.getString(PropertyKeys.updatePath) else conf.getString(PropertyKeys.tempDownloadPath)
+    val sgs = args.filterNot(s => s.contains(updateFlag) || s.contains(passwordFlag)).toList
+    new Args(update, user, password, downloadPath, sgs)
+  }
+}
+
 object Main {
 
   def main(args: Array[String]) = {
     args.length match {
-      case 0 => println("invalid arguments, please write: sgNames* [--u]")
+      case 0 => println("invalid arguments, please write: sgNames* [--u] --pPwd")
       case _ => {
-        val update = args.contains("--u")
-        val conf = ConfigFactory.load()
-        val user = conf.getString(PropertyKeys.user)
-        val pwd = conf.getString(PropertyKeys.pwd)
 
-        val downloadPath = if (update) conf.getString(PropertyKeys.updatePath) else conf.getString(PropertyKeys.tempDownloadPath)
-        val sgs = args.filterNot(_.equals("--u"))
-
+        val parsedArgs = Args.parseArgs(args)
         val sgClient = new SGClient(true)
 
         try {
 
-          val startMessageString = if (update) "updating: %s\n===========================" else "downloading: %s\n==========================="
+          val startMessageString = if (parsedArgs.update) "updating: %s\n===========================" else "downloading: %s\n==========================="
           val endMessageString = "finished: %s\n==========================="
 
-          sgClient.login(user, pwd)
-          val downloaders = sgs map { sg => new Downloader(sg, sgClient, new LevelOfReporting(2)) }
+          sgClient.login(parsedArgs.user, parsedArgs.pwd)
+          val downloaders = parsedArgs.sgs map { sg => new Downloader(sg, sgClient, new LevelOfReporting(2)) }
           downloaders foreach { d =>
             println(startMessageString.format(d.sgName))
-            d.download(downloadPath)
+            d.download(parsedArgs.downloadPath)
             println(endMessageString.format(d.sgName))
           }
         } catch {
