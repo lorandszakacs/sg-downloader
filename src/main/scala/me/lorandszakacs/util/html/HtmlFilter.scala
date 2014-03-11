f6f06305520bc8107d141bd148dcbbb1fab667c3
Lorand Szakacs <lorand.szakacs@gmail.com>
2014-03-11 10:22:36 -0500

    Added HtmlFilters
    Added a neat feature where an HtmlParser can take several filters
    that can be combined with the && operator.
    
    For instance:
    ```
    val parser = HtmlParser(someData)
    parser filter Class("foo") && LinkFilter()
    ```
    The above code would first grab all HTML elements that have the class
    foo and then it would filter out all the links from it.

--- src/main/scala/me/lorandszakacs/util/html/HtmlFilter.scala
+++ src/main/scala/me/lorandszakacs/util/html/HtmlFilter.scala
@@ -1,3 +1,26 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013 Lorand Szakacs
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package me.lorandszakacs.util.html
 
 import scala.collection.mutable.ListBuffer
@@ -5,61 +28,90 @@ import scala.collection.mutable.ListBuffer
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 
+/**
+ * @author lorand
+ *
+ */
 sealed trait HtmlFilter {
-  def &&(first: HtmlFilter, second: HtmlFilter): HtmlFilter = CompositeFilter(first, second)
+  def &&(that: HtmlFilter): HtmlFilter = CompositeFilter(this, that)
 
-  def apply(doc: Document): List[Element]
+  def apply(doc: Document): List[String] = {
+    val elements = filter(doc)
+    elementsToList(elements) map (_.toString)
+  }
 
-  def apply(htmlChunk: String): List[Element]
+  protected final def elementsToList(elements: Elements): List[Element] = {
+    val buff = ListBuffer[Element]();
+    val iterator = elements.iterator()
+    while (iterator.hasNext()) {
+      buff.append(iterator.next())
+    }
+    buff.toList
+  }
+
+  def apply(htmlChunk: String): List[String] = {
+    val doc = Jsoup.parseBodyFragment(htmlChunk)
+    apply(doc)
+  }
+
+  protected def filter(doc: Document): Elements
 }
 
+/**
+ * @author lorand
+ *
+ */
 private case class CompositeFilter(val first: HtmlFilter, val second: HtmlFilter) extends HtmlFilter {
-
-  override def apply(doc: Document): List[Element] = {
+  override def apply(doc: Document): List[String] = {
     val resultsFirst = first(doc)
-    val resultsSecond = resultsFirst.map(el => second(el.html()))
+    val resultsSecond = resultsFirst.map(el => second(el))
     resultsSecond.flatten
   }
 
-  override def apply(htmlChunk: String): List[Element] = {
+  override def apply(htmlChunk: String): List[String] = {
     val resultsFirst = first(htmlChunk)
-    val resultsSecond = resultsFirst.map(el => second(el.html()))
+    val resultsSecond = resultsFirst.map(el => second(el))
     resultsSecond.flatten
   }
+
+  override def filter(doc: Document): Elements = null
 }
 
-case class Attribute(private val attribute: String) extends HtmlFilter {
-
-  override def apply(doc: Document): List[Element] = {
-    val elements = doc.getElementsByAttribute(attribute)
-    val buff = ListBuffer[Element]();
-    val iterator = elements.iterator()
-    while (iterator.hasNext()) {
-      buff.append(iterator.next())
-    }
-    buff.toList
-  }
-
-  override def apply(htmlChunk: String): List[Element] = {
-    val doc = Jsoup.parseBodyFragment(htmlChunk)
-    this.apply(doc)
-  }
+/**
+ * @author lorand
+ *
+ */
+case class AttributeFilter(private val attribute: String) extends HtmlFilter {
+  override def filter(doc: Document): Elements = doc.getElementsByAttribute(attribute)
 }
 
-case class Class(private val className: String) extends HtmlFilter {
-  override def apply(doc: Document): List[Element] = {
-    val elements = doc.getElementsByClass(className)
-    val buff = ListBuffer[Element]();
-    val iterator = elements.iterator()
-    while (iterator.hasNext()) {
-      buff.append(iterator.next())
-    }
-    buff.toList
+/**
+ * @author lorand
+ *
+ */
+case class ClassFilter(private val className: String) extends HtmlFilter {
+  override def filter(doc: Document): Elements = doc.getElementsByClass(className)
+}
+
+/**
+ * @author lorand
+ *
+ */
+case class TagFilter(private val tagName: String) extends HtmlFilter {
+  override def filter(doc: Document): Elements = doc.getElementsByTag(tagName)
+}
+
+case class LinkFilter() extends HtmlFilter {
+  private final val BetweenQuotesRegex = "\"(.*?)\"".r
+
+  override def apply(doc: Document): List[String] = {
+    val hrefAttributes = filter(doc)
+    val elementsWithHref = elementsToList(hrefAttributes)
+    val links = elementsWithHref map (e => e.attr("href").toString)
+    links.toList
   }
 
-  override def apply(htmlChunk: String): List[Element] = {
-    val doc = Jsoup.parseBodyFragment(htmlChunk)
-    this.apply(doc)
-  }
+  override def filter(doc: Document): Elements = doc.getElementsByAttribute("href")
 }
