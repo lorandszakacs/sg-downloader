36f048c92b9d2276150b9683efc068ac06d60d73
Lorand Szakacs <lorand.szakacs@busymachines.com>
2014-08-17 22:45:52 +0300

    Added fileSystemPaths to the model

--- src/main/scala/com/lorandszakacs/sgd/model/SuicideGirl.scala
+++ src/main/scala/com/lorandszakacs/sgd/model/SuicideGirl.scala
@@ -5,16 +5,28 @@ import java.time.LocalDate
 
 case class SuicideGirl(
   val uri: Uri,
-  val name: String)
+  val name: String) {
+
+  val path = name
+}
 
 case class PhotoSet(
   val uri: Uri,
   val name: String,
   val photos: List[Photo],
-  val containingSuicideGirl: SuicideGirl,
-  val date: LocalDate)
+  val date: LocalDate,
+  val ownerSuicideGirl: SuicideGirl) {
+
+  private val readableDate = s"${date.getYear()}.${date.getMonth()}.${date.getDayOfMonth()}"
+
+  val normalizedName = name.replace(" ", "-").replace("?", "-").replace("/", "-").replace("\\", "-")
+  val path = s"${ownerSuicideGirl.path}/${readableDate}-${normalizedName}"
+}
 
 case class Photo(
   val uri: Uri,
-  val index: String,
-  val containingPhotoSet: PhotoSet)
+  val index: Int,
+  val containingPhotoSet: PhotoSet) {
+
+  val path = s"${containingPhotoSet.path}/${containingPhotoSet.path.replace("/", ".")}-${index}"
+}
