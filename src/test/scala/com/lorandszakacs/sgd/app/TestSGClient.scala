5b4d43f275f8342083e80a5f843fd2ccb699db15
Lorand Szakacs <lorand.szakacs@busymachines.com>
2014-08-22 02:01:47 +0300

    Added test for photoalbum fetching

--- src/test/scala/com/lorandszakacs/sgd/app/TestSGClient.scala
+++ src/test/scala/com/lorandszakacs/sgd/app/TestSGClient.scala
@@ -1,24 +1,17 @@
 package com.lorandszakacs.sgd.app
 
-import scala.concurrent.Await
-import scala.concurrent.duration.DurationInt
 import scala.io.StdIn
 import scala.language.postfixOps
 import scala.util.{ Failure, Success }
-import com.lorandszakacs.commons.html._
 import com.lorandszakacs.sgd.http.SGClient
 import akka.actor.ActorSystem
-import spray.http.Uri.apply
 import spray.http.Uri
-import java.time.LocalDate
-import com.lorandszakacs.sgd.model._
-import com.lorandszakacs.sgd.client.data.PhotoSetPage
-import com.lorandszakacs.sgd.http.Parser
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 object TestSGClient extends App {
   implicit val system = ActorSystem("test-login-client")
   import system.dispatcher
-  //partial=false&offset=33
 
   val user = { print("user:"); StdIn.readLine() }
   val pwd = { print("pwd:"); val result = StdIn.readLine(); println(); result }
@@ -29,6 +22,14 @@ object TestSGClient extends App {
     case Failure(e) =>
       throw e
   }
+
+  val sashPhotoSetsPage = Uri("https://suicidegirls.com/girls/sash/photos/view/photosets/")
+
+  def sashAlbums = {
+    Await.result(sgClient.getPhotoSetUris(sashPhotoSetsPage), 1 minute).get
+  }
+
+  println(sashAlbums.mkString("\n"))
 
   system.shutdown()
 }
