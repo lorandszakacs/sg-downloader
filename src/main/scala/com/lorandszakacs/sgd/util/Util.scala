24a157eb9343abf5c2235cb4ffb41de2fbec3161
Lorand Szakacs <lorand.szakacs@busymachines.com>
2014-08-21 21:59:49 +0300

    Made the time parsing try a pattern that does not include the year

--- src/main/scala/com/lorandszakacs/sgd/util/Util.scala
+++ src/main/scala/com/lorandszakacs/sgd/util/Util.scala
@@ -5,6 +5,12 @@ import java.time.LocalDate
 import scala.util.{ Failure, Success, Try }
 
 object Util {
+
+  val months = Map(1 -> "Jan", 2 -> "Feb", 3 -> "Mar",
+    4 -> "Apr", 5 -> "May", 6 -> "Jun",
+    7 -> "Jul", 8 -> "Aug", 9 -> "Sep",
+    10 -> "Oct", 11 -> "Nov", 12 -> "Dec").map(p => p._2 -> p._1)
+
   def parseStringToLocalDate(t: String): Try[LocalDate] = {
     val time = t.trim()
     try {
@@ -12,16 +18,22 @@ object Util {
       val datePattern = """(\w\w\w) (\d*), (\d\d\d\d)""".r
       val datePattern(month, day, year) = time
 
-      val months = Map(1 -> "Jan", 2 -> "Feb", 3 -> "Mar",
-        4 -> "Apr", 5 -> "May", 6 -> "Jun",
-        7 -> "Jul", 8 -> "Aug", 9 -> "Sep",
-        10 -> "Oct", 11 -> "Nov", 12 -> "Dec").map(p => p._2 -> p._1)
-      val monthAsInt = months("Aug")
+      val monthAsInt = months(month)
 
       val localDate = LocalDate.of(year.toInt, monthAsInt, day.toInt)
       Success(localDate)
     } catch {
-      case e: Throwable => Failure(e)
+      case e: Throwable => {
+        try {
+          val simplifiedDatePattern = """(\w\w\w) (\d*)""".r
+          val simplifiedDatePattern(month, day) = time
+          val monthAsInt = months(month)
+          val localDate = LocalDate.of(monthAsInt, day.toInt, LocalDate.now().getYear())
+          Success(localDate)
+        } catch {
+          case e: Throwable => Failure(e)
+        }
+      }
     }
   }
 }
