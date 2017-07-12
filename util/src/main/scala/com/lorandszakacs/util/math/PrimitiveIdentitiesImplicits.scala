ebae32d8041be04c481571167e1ac82eb61ea6b9
Lorand Szakacs <lsz@lorandszakacs.com>
2017-07-12 21:58:13 +0300

    Add Identity typeclass

---
+++ util/src/main/scala/com/lorandszakacs/util/math/PrimitiveIdentitiesImplicits.scala
@@ -0,0 +1,19 @@
+package com.lorandszakacs.util.math
+
+/**
+  *
+  * @author Lorand Szakacs, lsz@lorandszakacs.com, lorand.szakacs@busymachines.com
+  * @since 12 Jul 2017
+  *
+  */
+trait PrimitiveIdentitiesImplicits {
+
+  implicit val intId: Identity[Int] = new Identity[Int] {
+    override def identifiesAs(x: Int, y: Int): Boolean = x == y
+  }
+
+  implicit val stringId: Identity[String] = new Identity[String] {
+    override def identifiesAs(x: String, y: String): Boolean = x == y
+  }
+
+}
