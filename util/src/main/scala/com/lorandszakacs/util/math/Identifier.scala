da7b53dd28bcbc48a73c2cb4d72280f384f0ee06
Lorand Szakacs <lsz@lorandszakacs.com>
2017-07-14 23:58:13 +0300

    Add identifier type-class

---
+++ util/src/main/scala/com/lorandszakacs/util/math/Identifier.scala
@@ -0,0 +1,17 @@
+package com.lorandszakacs.util.math
+
+/**
+  *
+  * @author Lorand Szakacs, lsz@lorandszakacs.com, lorand.szakacs@busymachines.com
+  * @since 14 Jul 2017
+  *
+  */
+trait Identifier[Type, IdType] {
+  def id(t: Type): IdType
+}
+
+object Identifier {
+  def apply[Type, IdType](f: Type => IdType) = new Identifier[Type, IdType] {
+    override def id(t: Type): IdType = f(t)
+  }
+}
