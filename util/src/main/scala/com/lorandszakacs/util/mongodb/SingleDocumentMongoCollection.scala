24935a582c4046f1fb1fda3b176edd67de34079a
Lorand Szakacs <lsz@lorandszakacs.com>
2017-07-14 22:56:49 +0300

    SingleDocumentMongoCollection now uses uniqueID

--- util/src/main/scala/com/lorandszakacs/util/mongodb/SingleDocumentMongoCollection.scala
+++ util/src/main/scala/com/lorandszakacs/util/mongodb/SingleDocumentMongoCollection.scala
@@ -5,6 +5,7 @@ import com.lorandszakacs.util.future._
 /**
   *
   * A convenience trait for collections that have one single document
+  * with a unique, and stable ID.
   *
   * @author Lorand Szakacs, lsz@lorandszakacs.com, lorand.szakacs@busymachines.com
   * @since 14 Jul 2017
@@ -14,11 +15,23 @@ trait SingleDocumentMongoCollection[Entity, IdType, BSONTargetType <: BSONValue]
 
   protected implicit def executionContext: ExecutionContext
 
-  protected implicit def objectHandler: BSONDocumentHandler[Entity]
+  protected def objectHandler: BSONDocumentHandler[Entity]
 
   protected implicit def idHandler: BSONHandler[BSONTargetType, IdType]
 
+  protected implicit def objectHandlerWithUniqueId: BSONDocumentHandler[Entity] = {
+    new BSONDocumentReader[Entity] with BSONDocumentWriter[Entity] with BSONHandler[BSONDocument, Entity] {
+      override def write(t: Entity): BSONDocument =
+        objectHandler.write(t).++(_id -> idHandler.write(uniqueDocumentId))
+
+      override def read(bson: BSONDocument): Entity =
+        objectHandler.read(bson)
+    }
+  }
+
   protected def uniqueDocumentId: IdType
+
+  protected def defaultEntity: Entity
 
   protected def db: Database
 
@@ -35,13 +48,12 @@ trait SingleDocumentMongoCollection[Entity, IdType, BSONTargetType <: BSONValue]
   def createOrUpdate(e: Entity): Future[Unit] =
     repo.create(e)
 
-  def rewrite(e: Entity): Future[Unit] =
-    repo.createOrUpdate(idQuery, e)
-
   def remove(): Future[Unit] =
     repo.remove(idQuery)
 
   def find: Future[Option[Entity]] =
     repo.findOne(idQuery)
+
+  def get: Future[Entity] = this.find.map(_.getOrElse(defaultEntity))
 
 }
