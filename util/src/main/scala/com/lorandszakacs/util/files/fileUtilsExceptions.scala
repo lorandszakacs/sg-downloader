85ef5efecac3546555b9655f965c3664277a1f09
Lorand Szakacs <lsz@lorandszakacs.com>
2017-07-14 20:21:34 +0300

    Add FileUtils to util

---
+++ util/src/main/scala/com/lorandszakacs/util/files/fileUtilsExceptions.scala
@@ -0,0 +1,21 @@
+package com.lorandszakacs.util.files
+
+/**
+  *
+  * @author Lorand Szakacs, lsz@lorandszakacs.com
+  * @since 17 Jul 2016
+  *
+  */
+final case class RootFolderCouldNotBeOpenedException(folderPath: String, cause: Throwable) extends Exception(
+  s"Root folder: $folderPath could not be cleaned because: ${cause.getMessage}",
+  cause
+)
+
+final case class RootFolderFileCouldNotBeDeleted(fp: String, cause: Throwable) extends Exception(
+  s"file: $fp could not be deleted because: ${cause.getMessage}",
+  cause
+)
+
+final case class FailedToCreateFolderException(fp: String) extends Exception(
+  s"failed to create folder: $fp."
+)
