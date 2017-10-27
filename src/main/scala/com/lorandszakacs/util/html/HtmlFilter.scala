0cddb0365bccdd13daa413140c121857b8166e9c
Lorand Szakacs <lsz@lorandszakacs.com>
2017-10-27 17:36:48 +0300

    Bump sbt version

--- src/main/scala/com/lorandszakacs/util/html/HtmlFilter.scala
+++ src/main/scala/com/lorandszakacs/util/html/HtmlFilter.scala
@@ -1,19 +1,19 @@
 /**
- * Copyright 2015 Lorand Szakacs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+  * Copyright 2015 Lorand Szakacs
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  */
 package com.lorandszakacs.util.html
 
 import org.jsoup.Jsoup
@@ -23,10 +23,10 @@ import org.jsoup.select.Elements
 import scala.collection.mutable.ListBuffer
 
 /**
- * @author Lorand Szakacs, lsz@lorandszakacs.com
- * @since 16 Mar 2015
- *
- */
+  * @author Lorand Szakacs, lsz@lorandszakacs.com
+  * @since 16 Mar 2015
+  *
+  */
 protected sealed trait HtmlFilter {
   def &&(that: HtmlFilter): HtmlFilter = CompositeFilter(this, that)
 
